7a497f6d2032ab9f1f10a2d35b673d063fe42a03
David Roldán Nocelo <david.roldan.nocelo@gmail.com>
2016-05-05 20:32:22 +0200

    Añadido: tiendas, sliders, botones, estilos botones navbar

--- Cliente/www/js/index_user_scripts.js
+++ Cliente/www/js/index_user_scripts.js
@@ -24,7 +24,13 @@
     });
 
         /* button  Button */
-    $(document).on("click", ".uib_w_4", function(evt)
+
+
+        /* button  #navBtiendas */
+
+
+        /* button  #navBtiendas */
+    $(document).on("click", "#navBtiendas", function(evt)
     {
          /*global uib_sb */
          /* Other possible functions are:
@@ -34,7 +40,46 @@
             uib_sb.close_all_sidebars()
           See js/sidebar.js for the full sidebar API */
 
-         uib_sb.toggle_sidebar($("#sidebar"));
+         uib_sb.toggle_sidebar($(".uib_w_10"));
+    });
+
+        /* button  #navBPrinci */
+    $(document).on("click", "#navBPrinci", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are:
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+
+         uib_sb.toggle_sidebar($("#sbPrin"));
+    });
+
+        /* button  Botigues */
+
+
+        /* button  Promos */
+
+
+        /* button  #bTp */
+
+
+        /* button  #bTp */
+
+
+        /* button  #bPp */
+    $(document).on("click", "#bPp", function(evt)
+    {
+        /* your code goes here */
+    });
+
+        /* button  #bTp */
+    $(document).on("click", "#bTp", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#tiendas");
     });
 
     }
